7acaa19094d36ae141936f8c768201d5ad56436b
ccantu20 <carloscq920@gmail.com>
2022-01-06 17:28:42 -0600

    Created the search page and implimented the bayut api within it to display properties using some default search parameters

---
+++ my-app/components/SearchFilters.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import { Flex, Box, Text, Select, Input, Spinner, Icon, Button } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+import Image from 'next/image'
+
+export default function SearchFilters() {
+  return (
+    <div>
+      searchFilters
+    </div>
+  )
+}
